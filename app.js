6468a28feee4b818bec13dd6ae44dcb8d664078f
aaroncalero <aaroncl80@gmail.com>
2021-03-17 01:35:15 +0000

    subiendo test 1

---
+++ app.js
@@ -0,0 +1,16 @@
+
+//const sum = (a,b) => {
+  //  return a + b
+//}
+
+// just a console log for ourselves.
+//console.log(7,3)
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
+//module.exports = { sum };
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
